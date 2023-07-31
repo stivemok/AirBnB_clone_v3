d3860970ebb02a4916459671348edc23071664e1
stivemok <stivemok@gmail.com>
2023-07-31 14:19:24 -0700

    head conflict removed

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """Blueprint for API"""
 from flask import Blueprint
-<<<<<<< HEAD
-=======
-
->>>>>>> e7851eb22dddb04d5af4b1ac6691b7ee52da657b
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -14,9 +10,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> e7851eb22dddb04d5af4b1ac6691b7ee52da657b
