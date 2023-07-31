e7851eb22dddb04d5af4b1ac6691b7ee52da657b
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-07-31 22:55:05 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """Blueprint for API"""
 from flask import Blueprint
 
-
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -13,4 +13,4 @@ from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
