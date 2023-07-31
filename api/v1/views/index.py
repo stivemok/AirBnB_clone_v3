282a3f50be20a018ea00aec88fc308520ccf5e78
stivemok <stivemok@gmail.com>
2023-07-31 15:08:45 -0700

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,19 +11,13 @@ from models.user import User
 from api.v1.views import app_views
 
 
-@app_views.route('/', methods=['GET'], strict_slash=False)
-def Hello_world():
-    '''Gets the status of the API'''
-    return ("Hello_world")
-
-
-@app_views.route('/status', methods=['GET'], strict_slash=False)
+@app_views.route('/status')
 def get_status():
     '''Gets the status of the API'''
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slash=False)
+@app_views.route('/stats')
 def get_stats():
     """retrives the number of each objects by type"""
     objects = {
